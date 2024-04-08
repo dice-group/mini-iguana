f6b1f4fb12fad4bf9ba7daaaad78eeb455f7f110
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2024-04-08 15:32:26 +0200

    switch to blocking api

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use anyhow::Context;
+use bytes::Bytes;
 use clap::Parser;
-use reqwest::{Client, Url};
+use reqwest::{blocking::Client, Url};
 use std::{
-    io::Write,
+    io::{BufRead, BufWriter, Write},
     path::PathBuf,
     time::{Duration, Instant},
 };
-use bytes::Bytes;
-use tokio::{
-    fs::File,
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-};
-use tokio::io::BufWriter;
 
 #[derive(Parser)]
 struct Opts {
@@ -22,13 +17,10 @@ struct Opts {
     timeout_secs: Option<u64>,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     let Opts { endpoint, update_query_file, timeout_secs } = Opts::parse();
 
-    tracing_subscriber::fmt()
-        .with_writer(std::io::stderr)
-        .finish();
+    tracing_subscriber::fmt().with_writer(std::io::stderr).init();
 
     let mut builder = Client::builder().tcp_nodelay(true);
 
@@ -38,61 +30,46 @@ async fn main() -> anyhow::Result<()> {
 
     let client = builder.build().unwrap();
 
-    let mut f = BufReader::new(
-        File::open(update_query_file)
-            .await
-            .context("Unable to open query file")?,
-    )
-    .lines();
+    let mut f =
+        std::io::BufReader::new(std::fs::File::open(update_query_file).context("Unable to open query file")?).lines();
 
-    let mut stdout = BufWriter::new(tokio::io::stdout());
-    stdout
-        .write_all(b"query_id,runtime_secs")
-        .await
-        .context("Cannot write to stdout")?;
-
-    let mut output_buf = Vec::new();
+    let mut stdout = BufWriter::new(std::io::stdout().lock());
+    writeln!(stdout, "query_id,runtime_secs").context("Unable to write to stdout")?;
 
     let mut id = 0;
-    while let Some(query) = f.next_line().await.context("Unable to read query file")? {
+    while let Some(Ok(query)) = f.next() {
         let start_time = Instant::now();
 
-        match run_query(&client, &endpoint, query).await {
+        match run_query(&client, &endpoint, query) {
             Ok(resp) => {
                 std::hint::black_box(resp);
 
                 let end_time = Instant::now();
                 let runtime_secs = end_time.duration_since(start_time).as_secs_f64();
 
-                output_buf.clear();
-                writeln!(output_buf, "{id},{runtime_secs}").unwrap();
+                writeln!(stdout, "{id},{runtime_secs}").context("Unable to write to stdout")?;
             },
             Err(e) => {
                 tracing::warn!("HTTP request failed: {e:#?}");
-
-                output_buf.clear();
-                writeln!(output_buf, "{id},{}", f64::INFINITY).unwrap();
-            }
+                writeln!(stdout, "{id},{}", f64::INFINITY).context("Unable to write to stdout")?;
+            },
         }
 
-        stdout.write_all(&output_buf).await.context("Cannot write to stdout")?;
         id += 1;
     }
 
     Ok(())
 }
 
-async fn run_query(client: &Client, endpoint: &Url, query: String) -> anyhow::Result<Bytes> {
+fn run_query(client: &Client, endpoint: &Url, query: String) -> anyhow::Result<Bytes> {
     client
         .post(endpoint.clone())
         .header("Content-Type", "application/sparql-update")
         .body(query)
         .send()
-        .await
         .context("Error sending HTTP request")?
         .error_for_status()
         .context("Received error HTTP response")?
         .bytes()
-        .await
         .context("Unable to receive HTTP response body")
 }
